cf6d09db22da6748c93cdecb9923656d1f20905e
beyonddream <43626691+beyonddream@users.noreply.github.com>
2021-12-09 18:21:38 -0800

    * day 6, part 2

--- day6.py
+++ day6.py
@@ -2,16 +2,18 @@
 
 import sys
 
+MAX_ITERATION = 256
+
 def solve():
     with open("inputs/day6.txt") as file:
         data = [line for line in file]
-        solve_part_1(data)
+        # WARNING: This will take forever for max_iteration = 256
+        #solve_part_1(data)
         solve_part_2(data)
 
 def solve_part_1(data):
     fishes = list(map(int, filter(None, data[0].split(','))))
-    print("number of initial fishes is {}".format(fishes))
-    max_iteration = 80
+    max_iteration = MAX_ITERATION
     for day in range(1, max_iteration+1):
         new_fishes = []
         for idx, fish in enumerate(fishes):
@@ -21,11 +23,36 @@ def solve_part_1(data):
             else:
                 fishes[idx] -= 1
         fishes.extend(new_fishes)
-    print("After {} day there are {} fishes".format(max_iteration, len(fishes)))
+    print("After {} day there are {} fishes. fishes {}".format(max_iteration, len(fishes),
+                                                   fishes))
 
 
 def solve_part_2(data):
-    pass
+    timers = list(map(int, filter(None, data[0].split(','))))
+    max_iteration = MAX_ITERATION
+    # represent each of the fish into 9 buckets (0 to 8)
+    internal_timer_bucket = [0] * 9
+    for timer in timers:
+        internal_timer_bucket[timer] += 1
+
+    for day in range(1, max_iteration+1):
+        new_fishes = 0
+        if internal_timer_bucket[0] != 0:
+            new_fishes = internal_timer_bucket[0]
+        # first move the fishes
+        max_timer = len(internal_timer_bucket)
+        for idx in range(max_timer - 1):
+            internal_timer_bucket[idx] = internal_timer_bucket[idx+1]
+        # now move new_fishes to 8 and add to 6
+        if new_fishes > 0:
+            internal_timer_bucket[-1] = new_fishes
+            internal_timer_bucket[6] += new_fishes
+        else:
+            internal_timer_bucket[-1] = 0
+
+    print("After {} day there are {} fishes. fishes {}".format(max_iteration,
+                                                    sum(internal_timer_bucket),
+                                                              internal_timer_bucket))
 
 if __name__ == '__main__':
     solve()
