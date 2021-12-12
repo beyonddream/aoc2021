2473139f1602180a8d7d92a8eb23fa1f8f43e15f
beyonddream <43626691+beyonddream@users.noreply.github.com>
2021-12-11 16:31:28 -0800

    * day 7 , part 2

--- day7.py
+++ day7.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 def solve():
     with open("inputs/day7.txt") as file:
@@ -11,7 +12,6 @@ def solve():
 def solve_part_1(data):
     crab_positions = list(map(int, filter(None, data[0].split(','))))
     sorted_crab_positions = sorted(crab_positions)
-    print("sorted crab positions = {}".format(sorted_crab_positions))
     no_of_crabs = len(sorted_crab_positions)
     def get_total_fuel_cost(aligned_pos_idx, positions):
         total_cost = 0
@@ -32,7 +32,25 @@ def solve_part_1(data):
     return total_fuel_cost
 
 def solve_part_2(data):
-    pass
+    crab_positions = list(map(int, filter(None, data[0].split(','))))
+    def get_total_fuel_cost(max_aligned_pos, positions):
+        min_total_cost = math.inf
+        for aligned_pos in range(max_aligned_pos + 1):
+            total_cost = 0
+            for idx, pos in enumerate(positions):
+                diff = abs(positions[idx] - aligned_pos)
+                total_cost += (diff * (diff + 1)) // 2
+            min_total_cost = min(min_total_cost, total_cost)
+        return min_total_cost
+    total_fuel_cost = 0
+    avg_crab_position = round(sum(crab_positions) / len(crab_positions))
+    # try all positions from 0 to max = avg_crab_position (inclusive) and find
+    # the minimum of all of these
+    total_fuel_cost = get_total_fuel_cost(avg_crab_position, crab_positions)
+    print("The total fuel spent to align to a position is\
+        {}".format(total_fuel_cost))
+
+    return total_fuel_cost
 
 if __name__ == '__main__':
     solve()
